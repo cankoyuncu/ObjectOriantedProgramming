a0ee0a202895ef3241999eeb33b85d63d958ece0
cankoyuncu <2110206542@ogrenci.karabuk.edu.tr>
2022-11-21 01:46:09 +0300

    ObjectOriantedProgramming

---
+++ week4/Week4SlideExample/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Time1 time = new Time1(25, 4, 23);
+        Time1 time2 = new Time1(12, 6, 24);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(time);
+        System.out.println(time2);
+
+    }
+}
